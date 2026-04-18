2a88256842c4d40ef22957f74495d0a4f32c2679
copernicmh <mhcopernic@gmail.com>
2026-04-17 22:19:57 -0400

    test

--- test.py
+++ test.py
@@ -6,11 +6,5 @@ w3 = -0.32
 z1 = 0.6
 z2 = 0.3
 z3 = 0.2
-Z_zeta = (z1 * np.cos(w1 * zeta))
-''' generate synthetic R (radial distance) 
-and Z (vertical height, relative to the horizontal center) 
-cylindrical coordinate data
 
-data = np.column_stack([psi_t_col, theta, zetad])
-np.savetxt("trajectories.txt", data)
-print("Done")
+Z_zeta = (z1 * np.cos(w1 * zeta))
